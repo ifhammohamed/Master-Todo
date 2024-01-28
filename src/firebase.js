ee80d4f0c6c35c5473d0c443190ad2c132e5085c
Mohamed MRI <mohamedmri.21@uom.lk>
2024-01-28 11:44:30 +0530

    Update Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDVNi1TtCBL7laFYSJkv7W0xuVqC6d9wfk",
-  authDomain: "master-todo-5e767.firebaseapp.com",
-  projectId: "master-todo-5e767",
-  storageBucket: "master-todo-5e767.appspot.com",
-  messagingSenderId: "1024333618867",
-  appId: "1:1024333618867:web:ea2da0868a83a6a19cdac6",
-  measurementId: "G-C42PXC20RP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
